e17a51192242776d562a7807be916e6969cd5ade
TobiasFuellmich <35760086+TobiasFuellmich@users.noreply.github.com>
2018-04-27 00:07:37 +0200

    Update stepper_move.php

--- html/stepper/stepper_move.php
+++ html/stepper/stepper_move.php
@@ -1,11 +1,9 @@
 <?php
-/*$options = [
-    'cost' => 11,
-];
-echo password_hash("", PASSWORD_BCRYPT, $options);
-if(password_verify ( $_POST['pw'], '')!=1){
+/*
+if($_POST['pw']!==""){
 	exit;
-}*/
+}
+*/
 // ss init|cancel|start init and cancel start and cancel script, start gives move command
 if($_POST['ss']==="init"){
 	$lines=shell_exec('ps -ef | grep -c "/var/www/html/stepper/step.py.lock"');
