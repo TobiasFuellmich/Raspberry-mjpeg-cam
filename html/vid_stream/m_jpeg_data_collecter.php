1fe1355d3fc5c14ff5972f58288e12100a16b62e
TobiasFuellmich <35760086+TobiasFuellmich@users.noreply.github.com>
2018-04-26 23:40:26 +0200

    Update m_jpeg_data_collecter.php

--- html/vid_stream/m_jpeg_data_collecter.php
+++ html/vid_stream/m_jpeg_data_collecter.php
@@ -1,5 +1,11 @@
-
 <?php
+/*$options = [
+    'cost' => 11,
+];
+echo password_hash("", PASSWORD_BCRYPT, $options);
+if(password_verify ( $_POST['pw'], '')!=1){
+	exit;
+}*/
 $lines=shell_exec('ps -ef | grep -c "/var/www/html/vid_stream/camera.py.lock"');
 if($_POST['com']==='start'){
 	if (substr($lines,0,1)==2){
