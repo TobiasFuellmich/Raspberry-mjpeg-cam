39447d36d7ee054976812f367f7a71be1e613343
TobiasFuellmich <35760086+TobiasFuellmich@users.noreply.github.com>
2018-04-27 00:07:03 +0200

    Update cam_step.php

--- html/stepper/cam_step.php
+++ html/stepper/cam_step.php
@@ -1,11 +1,9 @@
 <?php
-/*$options = [
-    'cost' => 11,
-];
-echo password_hash("", PASSWORD_BCRYPT, $options);
-if(password_verify ( $_POST['pw'], '')!=1){
+/*
+if($_POST['pw']!==""){
 	exit;
-}*/
+}
+*/
 if($_POST['dir']==="up"){
 	$lines=shell_exec('ps -ef | grep -c "/var/www/html/stepper/cam_step.py.lock"');
 	if (substr($lines,0,1)==2){
