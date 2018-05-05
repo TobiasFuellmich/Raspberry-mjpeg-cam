0da411d059378f261ae1f7f3ccc6e31de9420698
TobiasFuellmich <35760086+TobiasFuellmich@users.noreply.github.com>
2018-05-05 21:59:06 +0200

    Add files via upload

--- html/vid_stream/m_jpeg_data_collecter.php
+++ html/vid_stream/m_jpeg_data_collecter.php
@@ -1,9 +1,7 @@
 <?php
-/*
-if($_POST['pw']!==""){
+if($_POST['pw']!=='$2y$11$mqy8oNP5VxijYR5OBTj4O.hZxVqTojjct/q.1FjyynTthYQxO.Jni'){
 	exit;
 }
-*/
 $lines=shell_exec('ps -ef | grep -c "/var/www/html/vid_stream/camera.py.lock"');
 if($_POST['com']==='start'){
 	if (substr($lines,0,1)==2){
